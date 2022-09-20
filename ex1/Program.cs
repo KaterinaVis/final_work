eb9921fac91846e35b66938fab6a736389ed1ceb
KaterinaVis <katerina4484@mail.ru>
2022-09-20 21:49:27 +0500

    ex1

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[5] {"bjhvd","bj", "12", "123", "df12r"};
+string[] secondarray = new string[array.Length];
+
+FilterArray(array, secondarray);
+Console.WriteLine ($"Строки, длина которых меньше или равна 3м символам:"); 
+PrintArray(secondarray);
+
+void FilterArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length < 4)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write($"{array1[i]} ");
+    }
+    Console.WriteLine();
+}
